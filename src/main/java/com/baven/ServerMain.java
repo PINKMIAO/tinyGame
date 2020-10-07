c71bb655feaccc419d25b112ac4b3579a352c9d4
PINK_MIAO <1176085412@qq.com>
2020-10-07 20:41:59 +0800

    update

---
+++ src/main/java/com/baven/ServerMain.java
@@ -0,0 +1,45 @@
+package com.baven;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.ServerChannel;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+
+public class ServerMain {
+    public static void main(String[] args) {
+
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workGroup = new NioEventLoopGroup();
+        ServerBootstrap b = new ServerBootstrap();
+
+        b.group(bossGroup, workGroup)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel ch) throws Exception {
+                        ch.pipeline().addLast(
+                                new HttpServerCodec(),
+                                new WebSocketServerProtocolHandler("/websocket"),
+                                new TestMsgHandler()
+                        );
+                    }
+
+                });
+        try {
+            ChannelFuture f = b.bind(12345).sync();
+
+            if (f.isSuccess()) System.out.println("服务器连接成功");
+
+            f.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/baven/TestMsgHandler.java
@@ -0,0 +1,29 @@
+package com.baven;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.websocketx.BinaryWebSocketFrame;
+
+public class TestMsgHandler extends SimpleChannelInboundHandler<Object> {
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println("接收到了 msg, msg =>" + msg);
+
+        BinaryWebSocketFrame frame = (BinaryWebSocketFrame) msg;
+        ByteBuf byteBuf = frame.content();
+
+
+        byte[] byteArray = new byte[byteBuf.readableBytes()];
+        byteBuf.readBytes(byteArray);
+
+        System.out.println("收到的字节 = ");
+        for (byte b : byteArray) {
+            System.out.print(b);
+            System.out.print(", ");
+        }
+        System.out.println();
+
+    }
+}
